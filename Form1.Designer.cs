e7736412bbf3855bfee702af540f214a0a62a086
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-24 14:37:01 -0400

    Final version of the Gardening guide. It doesn't look very good, but it demonstrates how menu buttons work and how to change fonts and colors.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,9 +45,13 @@ namespace Chapter9_GardeningGuide
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
-            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,7 +65,7 @@ namespace Chapter9_GardeningGuide
             "Daisies",
             "Asters",
             "Mums"});
-            this.comboBoxFlowers.Location = new System.Drawing.Point(75, 117);
+            this.comboBoxFlowers.Location = new System.Drawing.Point(12, 108);
             this.comboBoxFlowers.Name = "comboBoxFlowers";
             this.comboBoxFlowers.Size = new System.Drawing.Size(151, 28);
             this.comboBoxFlowers.TabIndex = 0;
@@ -79,7 +83,7 @@ namespace Chapter9_GardeningGuide
             "Palm",
             "Pine",
             "Yew"});
-            this.lstBoxTrees.Location = new System.Drawing.Point(388, 128);
+            this.lstBoxTrees.Location = new System.Drawing.Point(200, 108);
             this.lstBoxTrees.Name = "lstBoxTrees";
             this.lstBoxTrees.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
             this.lstBoxTrees.Size = new System.Drawing.Size(150, 84);
@@ -91,7 +95,7 @@ namespace Chapter9_GardeningGuide
             // 
             // txtBoxFlower
             // 
-            this.txtBoxFlower.Location = new System.Drawing.Point(59, 369);
+            this.txtBoxFlower.Location = new System.Drawing.Point(12, 251);
             this.txtBoxFlower.Name = "txtBoxFlower";
             this.txtBoxFlower.Size = new System.Drawing.Size(129, 27);
             this.txtBoxFlower.TabIndex = 2;
@@ -99,7 +103,7 @@ namespace Chapter9_GardeningGuide
             // 
             // txtBoxTrees
             // 
-            this.txtBoxTrees.Location = new System.Drawing.Point(388, 302);
+            this.txtBoxTrees.Location = new System.Drawing.Point(200, 251);
             this.txtBoxTrees.Multiline = true;
             this.txtBoxTrees.Name = "txtBoxTrees";
             this.txtBoxTrees.Size = new System.Drawing.Size(150, 94);
@@ -109,7 +113,7 @@ namespace Chapter9_GardeningGuide
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(75, 85);
+            this.label1.Location = new System.Drawing.Point(12, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 20);
             this.label1.TabIndex = 4;
@@ -118,7 +122,7 @@ namespace Chapter9_GardeningGuide
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(388, 96);
+            this.label2.Location = new System.Drawing.Point(200, 85);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 20);
             this.label2.TabIndex = 5;
@@ -127,7 +131,7 @@ namespace Chapter9_GardeningGuide
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(59, 346);
+            this.label3.Location = new System.Drawing.Point(14, 228);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(127, 20);
             this.label3.TabIndex = 6;
@@ -136,7 +140,7 @@ namespace Chapter9_GardeningGuide
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(385, 279);
+            this.label4.Location = new System.Drawing.Point(200, 228);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(117, 20);
             this.label4.TabIndex = 7;
@@ -148,10 +152,12 @@ namespace Chapter9_GardeningGuide
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.formatToolStripMenuItem,
             this.formatToolStripMenuItem1,
-            this.helpToolStripMenuItem});
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(363, 28);
             this.menuStrip1.TabIndex = 8;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -176,6 +182,7 @@ namespace Chapter9_GardeningGuide
             this.fontToolStripMenuItem.Name = "fontToolStripMenuItem";
             this.fontToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
             this.fontToolStripMenuItem.Text = "Font";
+            this.fontToolStripMenuItem.Click += new System.EventHandler(this.fontToolStripMenuItem_Click);
             // 
             // toolStripSeparator1
             // 
@@ -187,6 +194,7 @@ namespace Chapter9_GardeningGuide
             this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
             this.colorToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
             this.colorToolStripMenuItem.Text = "Color";
+            this.colorToolStripMenuItem.Click += new System.EventHandler(this.colorToolStripMenuItem_Click);
             // 
             // helpToolStripMenuItem
             // 
@@ -194,21 +202,50 @@ namespace Chapter9_GardeningGuide
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
             this.helpToolStripMenuItem.Text = "&Help";
             // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // toolStrip1
             // 
+            this.toolStrip1.Enabled = false;
             this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Location = new System.Drawing.Point(0, 28);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(363, 25);
             this.toolStrip1.TabIndex = 9;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.Tag = "";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(14, 301);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(82, 20);
+            this.lblOutput.TabIndex = 10;
+            this.lblOutput.Text = "Add water!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(363, 361);
+            this.Controls.Add(this.lblOutput);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -221,7 +258,7 @@ namespace Chapter9_GardeningGuide
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Gardening Guide";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -247,8 +284,12 @@ namespace Chapter9_GardeningGuide
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolTip toolTip1;
-        private System.Windows.Forms.ToolTip toolTip2;
         private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblOutput;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*
+ * I encountered a weird problem with visual studio. I could not delete anything. It would
+ * cause a windows noise when I pushed the delete key. Alt + Enter fixed the problem. 
+ */
 namespace Chapter9_GardeningGuide
 {
     public partial class Form1 : Form
@@ -20,15 +23,53 @@ namespace Chapter9_GardeningGuide
         private void lstBoxTrees_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.txtBoxTrees.Text = " ";
-            foreach(int treeIndex in lstBoxTrees.SelectedIndices)
+            foreach (int treeIndex in lstBoxTrees.SelectedIndices)
             {
                 this.txtBoxTrees.Text += lstBoxTrees.Items[treeIndex] + " ";
             }
         }
-
+        
         private void comboBoxFlowers_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.txtBoxFlower.Text = this.comboBoxFlowers.SelectedItem.ToString();
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) // Displays version information when the about menu btn is clicked.
+        {
+            MessageBox.Show("Gardenning Guide Application\n\n\nVersion 10", "About Gardening");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) // Exits the application.
+        {
+            Application.Exit();
+        }
+
+        /*
+         * Example 9-19, pg 511
+         */
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e) // This is pretty cool
+        {
+            /*
+             * This is used to change the font of the text in the label. 
+             */
+            fontDialog1.Font = lblOutput.Font; // This retrieves the current font setting. 
+            if(fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                lblOutput.Font = fontDialog1.Font;
+            }
+        }
+        /*
+         * Example 9-19, pg 511
+         */
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e) // This is pretty cool
+        {
+            /*
+             * This is used to change the color of the text in the label.
+             */
+            colorDialog1.Color = lblOutput.ForeColor; // This retrieves the current color setting.
+            if(colorDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                lblOutput.ForeColor = colorDialog1.Color;
+            }
         }
     }
 }
